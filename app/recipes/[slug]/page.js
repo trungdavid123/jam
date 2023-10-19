90ff6709e77feebfe0bbdd7eb26775cd1ac9a0c2
David <trungdavid969@gmail.com>
2023-10-19 18:51:58 +0200

    fixed img err

--- app/recipes/[slug]/page.js
+++ app/recipes/[slug]/page.js
@@ -49,15 +49,15 @@ const RecipeDetails = async ({ params }) => {
     if (!recipe.length) return notFound();
 
     console.log(recipe)
-    const {featuredImage, title, cookingTime, ingredients, method} = recipe[0].fields; 
+    const { featuredImage, title, cookingTime, ingredients, method } = recipe[0].fields;
     return (
         <div>
             <div className="banner">
-                <Image 
-                    src={`https://${featuredImage.fields.file.url}`} 
-                    alt={title} 
-                    width={featuredImage.fields.file.details.image.width} 
-                    height={featuredImage.fields.file.details.image.height} 
+                <Image
+                    src={`https:${featuredImage.fields.file.url}`}
+                    alt={title}
+                    width={featuredImage.fields.file.details.image.width}
+                    height={featuredImage.fields.file.details.image.height}
                 />
                 <h2>{title}</h2>
             </div>
